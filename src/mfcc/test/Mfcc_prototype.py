fef2d8dc8f2b1a4f78f2b97d5b683d18bff0e4e2
JudasBricot <arthur.wuhrlin@telecom-paris.fr>
2023-02-21 08:56:37 +0100

    Ajout fonction mfcc et érification cohérence avec librosa

--- src/mfcc/test/Mfcc_prototype.py
+++ src/mfcc/test/Mfcc_prototype.py
@@ -166,3 +166,117 @@ def mfcc(audio, sample_rate:int, filter_num = 13, frame_duration = 20, recovery_
     plt.show()
     
     return dct_spectrum
+
+##---- Framing Audio ----##
+
+# On se restreins à un seul channel car wavfile.read renvoie un tableau contenant 1 ou deux channel
+sample_rate = 44100 #Hz
+audio, sample_rate = lr.load("src\mfcc\data\samples\A0vH.wav", sr=sample_rate)
+
+frame_duration = 20 #ms
+
+recovery_proportion = 0.5
+
+frame_length = int(frame_duration / 1000 * sample_rate)
+framed_audio = frame_audio(audio, frame_length, recovery_proportion)
+print(framed_audio.shape)
+lr_framed_audio = lr.util.frame(audio, frame_length=frame_length, hop_length=int(frame_length * (1 - recovery_proportion)))
+print(lr_framed_audio.shape)
+
+print("Showing windowed audio")
+plt.figure(figsize=(15,4))
+plt.plot(framed_audio[0])
+plt.plot(np.transpose(lr_framed_audio)[0])
+plt.grid(True)
+#plt.show()
+
+window = get_han_window(frame_length)
+lr_window = lr.filters.get_window('hann', frame_length, False)
+
+print("Showing window")
+plt.figure(figsize=(15,4))
+plt.plot(window)
+plt.plot(lr_window)
+plt.grid(True)
+#plt.show()
+
+##---- Applying window ----##
+
+windowed_audio = appl_window(framed_audio, window)
+lr_window_audio =  lr_window * framed_audio
+
+print("Showing windowed audio")
+plt.figure(figsize=(15,4))
+plt.plot(windowed_audio[0])
+plt.plot(lr_window_audio[0])
+plt.grid(True)
+#plt.show()
+
+##---- Apply FFT ----##
+
+print("Applying FFT")
+fft_audio = np.zeros(shape=(windowed_audio.shape[0],windowed_audio.shape[1] // 2 + 1), dtype='complex')
+lr_fft_audio = np.zeros(shape=(windowed_audio.shape[0],windowed_audio.shape[1] // 2 + 1), dtype='complex')
+print(lr.stft(windowed_audio[0],frame_length,window=np.ones,center=False).shape)
+print(lr_fft_audio.shape)
+for n in range(fft_audio.shape[0]):
+    print("{} / {}".format(n, fft_audio.shape[0]))
+    # On ne prend que la première moitié de la FFT car elle est repétée
+    fft_audio[n,:frame_length // 2 + 1] = np.fft.fft(windowed_audio[n])[:frame_length //2 + 1]
+    lr_fft_audio[n,:frame_length // 2 + 1] = np.transpose(lr.stft(windowed_audio[n],frame_length,window=np.ones,center=False))[0]
+
+sqr_fft = sqr_module(fft_audio)
+lr_sqr_fft = sqr_module(lr_fft_audio)
+
+print("Showing windowed audio")
+plt.figure(figsize=(15,4))
+plt.plot(sqr_fft[0])
+plt.plot(lr_sqr_fft[0])
+plt.grid(True)
+#plt.show()
+
+##---- Getting filters ----##
+
+filter_num = 13
+filters_points, mel_freqs = get_filter_points(0, sample_rate/2, filter_num, frame_length, sample_rate)
+filters = get_triangles(filters_points, filter_num)
+
+# Comme l'aire des triangles augmente avec la fréquence, on divise par celle-ci pour ne pas faire apparaitre de bruit
+enorm = 2.0 / (mel_freqs[2:filter_num+2] - mel_freqs[:filter_num])
+filters *= enorm[:, np.newaxis]
+
+lr_filters = lr.filters.mel(sr=sample_rate, n_fft=frame_length, n_mels=13,fmin=0, fmax=sample_rate / 2)
+
+plt.figure(figsize=(15,4))
+for n in range(filter_num):
+    plt.plot(filters[n])
+plt.figure(figsize=(15,4))
+for n in range(filter_num):
+    plt.plot(lr_filters[n])
+#plt.show()
+
+##---- Applying filters ----##
+
+filtered_spectrum = np.zeros(shape=(sqr_fft.shape[0],filter_num))
+print("Calculating filter contribution for {} filters".format(filter_num))
+for n in range(filtered_spectrum.shape[0]):
+    filtered_spectrum[n] = calculate_filter_contribution(sqr_fft[n], filters, filter_num)
+
+##---- Applying log ----##
+
+audio_log = 10.0 * np.log10(filtered_spectrum + 10e-3)
+
+##---- Applying dct ----##
+
+lr_dct_spectrum = np.zeros(shape=filtered_spectrum.shape)
+dct_spectrum = np.zeros(shape=filtered_spectrum.shape)
+for n in range(filtered_spectrum.shape[0]):
+    lr_dct_spectrum[n] = dct(audio_log[n])
+    dct_spectrum[n] = dct(audio_log[n])
+
+plt.figure(figsize=(15,4))
+plt.imshow(np.transpose(dct_spectrum), aspect='auto', origin='lower')
+plt.figure(figsize=(15,4))
+
+plt.imshow(lr.feature.mfcc(audio,n_mfcc = 13, hop_length=int((1-recovery_proportion) * frame_length)), aspect='auto', origin='lower')
+plt.show()
